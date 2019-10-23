f18f307bc3eee96860bcb7fc5676ae43e76f3739
Dennis Windham <denisps74@yahoo.com>
2019-10-22 21:07:59 -0600

    Main page skeleton + js logics, moved emblem files

---
+++ resources/js/scripts.js
@@ -0,0 +1,141 @@
+/* Collection of scripts used across the website */
+
+/* 
+### Reference ###
+jobData -- array, used to pass data from the database to the webpage
+	0: ID			(integer)
+	1: Title 		(string)
+	2: Description	(string)
+	3: Brief desc.	(string)
+	4: Difficulty 	(integer)
+	5: Payment		(integer)
+	6: Deadline		(string)
+*/
+
+var jobCounter = 0; //Tentative global counter, will probably be deprecated in the future in favor of database IDs
+var displayedJobs = []; //Keeps track of the displayed jobs by their IDs
+var jobBox = document.getElementById("jobsBox"); //Reference to the jobsBox element
+var focusBox = document.getElementById("focusBox");
+
+/* Main Page grid system columns
+0 -- left column, normal view
+1 -- middle column, normal view
+2 -- right column, normal view
+3 -- alternative middle column, focused view */
+var colList = [document.getElementById("col0"), document.getElementById("col1"), document.getElementById("col2"), document.getElementById("col3")]
+
+/* Populates the main page with jobs. 
+count -- takes the number of jobs to load */
+function populatePage(count) {
+	/* !!! There will have to be an algorithm to determine which jobs to display on load */
+	id = -1;
+
+	for (var i = 0; i < count; i++)
+		generateJob(id);
+}
+
+/* Adds a new job to the web page
+id -- takes an id of the job */
+function generateJob(id) {
+	jobData = fetchJob(-1); //Passing -1 for now
+	displayedJobs.push(jobData[0]); //Pushes job ID into the array
+	displayJob(jobData);
+}
+
+/* Displays a job on the page
+jobData -- takes an array with job info */
+function displayJob(jobData) {
+
+	/* Creates an element to store job data */
+	jobBox.innerHTML =
+		jobBox.innerHTML +
+		"<div class=\"p-2 bd-highlight\"> <button type=\"button\" class=\"btn btn-link btn-block\" data-toggle=\"tooltip\" data-placement=\"right\" title=\"Learn more\" onclick=\"focusJob(" +
+		jobData[0] +
+		")\"><div class=\"card bg-light text-dark\"> <div class=\"card-body\"><table class=\"table table-bordered\"><tr><th colspan=\"2\">" +
+		jobData[1] +
+		"</th></tr><tr><td>Difficulty: " +
+		jobData[4] +
+		"</td><td id=\"jobPay\">Payment: $" +
+		jobData[5] +
+		"</td></tr><tr><td colspan=\"2\">Deadline: " +
+		jobData[6] +
+		"</td></tr><tr><td colspan=\"2\">" +
+		jobData[3] +
+		"</td></tr></table></div></div></button></div>";
+}
+
+/* Replaces the contents of the screen with a job list */
+function focusJob(id) {
+	jobData = fetchJob(id);
+	toggleColumns(1);
+
+	focusBox.innerHTML = "<div class=\"p-2 bd-highlight\"><div class=\"card bg-light text-dark\"> <div class=\"card-body\"><table class=\"table table-bordered\"><tr><th colspan=\"2\">" +
+		jobData[1] +
+		"</th></tr><tr><td>Difficulty: " +
+		jobData[4] +
+		"</td><td id=\"jobPay\">Payment: $" +
+		jobData[5] +
+		"</td></tr><tr><td colspan=\"2\">Deadline: " +
+		jobData[6] +
+		"</td></tr><tr><td colspan=\"2\">" +
+		jobData[2] +
+		"</td></tr></table><div class=\"row\"><div class=\"col\"><button class=\"btn btn-dark\" onclick=\"toggleColumns(" +
+		0 +
+		")\">Back</button></div><div class=\"col\"><button class=\"btn btn-dark\" onclick=\"acceptJob(" +
+		id +
+		")\">Accept</button></div></div></div></div></div>";
+}
+
+/* Toggles main page column states 
+0 -- default view
+1 -- focused view */
+function toggleColumns(state) {
+	if (state) {
+		colList[0].style.display = "none";
+		colList[1].style.display = "none";
+		colList[2].style.display = "none";
+		colList[3].style.display = "block"
+	}
+	else {
+		colList[0].style.display = "block";
+		colList[1].style.display = "block";
+		colList[2].style.display = "block";
+		colList[3].style.display = "none"
+	}
+}
+
+/* Allows a student to accept a particular job */
+function acceptJob(id) {
+	alert("Accepting job " + id);
+}
+
+/* Retrieves a job from the database 
+id -- takes the identifier of a job within the database */
+function fetchJob(id) {
+
+	/* To be implemented
+		https://node-postgres.com/ 
+		
+		Sends a query to the database*/
+
+	jobData = dummyJobData(); //Returns dummy data for the time being
+	return jobData;
+}
+
+/* Adds a job to the database
+Takes an array of job data */
+function newJob(data) {
+	//Have to store a picture on the server and pass a local path?
+}
+
+/* Rolls dummy job data for the project demo */
+function dummyJobData() {
+	var title = ["Coders wanted", "!!!Test Title!!!", "Looking for a friend"];
+
+	var briefDesc = ["Test description.", "Yet another test description.", "A third test description."];
+
+	var desc = ["Test description.Test description.Test description.Test description.Test description.Test description.Test description.Test description.Test description.Test description.Test description.Test description.Test description.", "Yet another test description.Yet another test description.Yet another test description.Yet another test description.Yet another test description.Yet another test description.Yet another test description.Yet another test description.Yet another test description.", "A third test description.A third test description.A third test description.A third test description.A third test description.A third test description.A third test description.A third test description.A third test description.A third test description."];
+
+	jobCounter++;
+	return [jobCounter, title[Math.floor(Math.random() * 3)], desc[Math.floor(Math.random() * 3)], briefDesc[Math.floor(Math.random() * 3)], Math.floor(Math.random() * 5) + 1, Math.floor(Math.random() * 100) + 5, "4/16/2077"];
+}
