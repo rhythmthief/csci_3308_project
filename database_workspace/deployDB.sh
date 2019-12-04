2c498f336d0d6afea3c30d98d892af0da99d3e99
Dennis Windham <denisps74@yahoo.com>
2019-12-04 16:42:33 -0700

    Finished student side, started Heroku deployment

--- database_workspace/deployDB.sh
+++ database_workspace/deployDB.sh
@@ -5,9 +5,9 @@ declare -a QUERIES
 QUERIES[0]="DROP DATABASE IF EXISTS arbonsi_db;"
 QUERIES[1]="CREATE DATABASE arbonsi_db;"
 QUERIES[2]="create table if not exists job(id serial primary key,title varchar(50),description varchar(500),brief_description varchar(250),difficulty smallint,payment smallint,deadline varchar(50),tags varchar(10)[] NOT NULL,creator smallint,acceptor smallint);"
-QUERIES[3]="CREATE TABLE IF NOT EXISTS login_info(username VARCHAR(50) NOT NULL,password varchar(50) NOT NULL);"
-QUERIES[4]="create table if not exists employers(employer_name varchar(50),employer_id serial primary key,about_employer varchar(500),company varchar(50),employer_email varchar(50),created_jobs smallint[]);"
-QUERIES[5]="CREATE TABLE IF NOT EXISTS students(id serial primary key, student_name VARCHAR(50) NOT NULL,skills varchar(250),about_me varchar(500),school varchar(50),student_email varchar(50), accepted_jobs smallint[]);"
+QUERIES[3]="create table if not exists employers(employer_name varchar(50),employer_id serial primary key,about_employer varchar(500),company varchar(50),employer_email varchar(50),created_jobs smallint[]);"
+QUERIES[4]="CREATE TABLE IF NOT EXISTS students(id serial primary key, student_name VARCHAR(50) NOT NULL,skills varchar(250),about_me varchar(500),school varchar(50),student_email varchar(50), accepted_jobs smallint[]);"
+QUERIES[5]="create table if not exists login_info(username varchar(50) primary key, password varchar(50), usertype integer NOT NULL, userid integer NOT NULL);"
 QUERIES[6]="CREATE VIEW jobListing_preview AS SELECT id, title, difficulty, deadline, brief_description FROM job;"
 QUERIES[7]="CREATE VIEW jobListing_full AS SELECT id, title, difficulty, deadline, description,payment,tags,about_employer,company,employer_email,employer_name FROM job INNER JOIN employers ON job.creator=ANY(employers.created_jobs);"
 QUERIES[8]="CREATE VIEW employer_profile AS SELECT employer_name, company, about_employer, employer_email FROM employers;"
@@ -31,25 +31,52 @@ then
      # Generating database entries
      for ((i = 1; i < 21; i++))
      do
+          #Rolling tags
+          tags="{"
+          if [[ $((RANDOM % 2)) == 1 ]]
+          then
+               tags=$tags"JavaScript,"
+          fi
+          if [[ $((RANDOM % 2)) == 1 ]]
+          then
+               tags=$tags"C#,"
+          fi
+          if [[ $((RANDOM % 2)) == 1 ]]
+          then
+               tags=$tags"LaTeX,"
+          fi
+          
+          # Edge case when we rolled no tags: there has to be at least one
+          if [[ $tags == "{" ]]
+          then
+               tags="{JavaScript,"
+          fi
+          
+          # Deleting the comma at the end and closes the brackets
+          tags=${tags::-1}"}"
+          
           # Inserts job entries
-          psql -U postgres -d arbonsi_db -c "INSERT INTO job(title, description, brief_description, difficulty, payment, deadline, tags, creator) VALUES ('Job $i', 'A long description of the job. The student will be able to admire the great effort put into the extremely detailed and sophisticated details regarding the project requirements.', 'Brief description for the preview.', $(($RANDOM % 5+1)), $(($RANDOM % 1000)), '01-01-2077', '{Node,C#}',$i);"
+          psql -U postgres -d arbonsi_db -c "INSERT INTO job(title, description, brief_description, difficulty, payment, deadline, tags, creator) VALUES ('Job $i', 'A long description of the job. The student will be able to admire the great effort put into the extremely detailed and sophisticated details regarding the project requirements.', 'Brief description for the preview.', $(($RANDOM % 5+1)), $(($RANDOM % 1000)), '01-01-2077', '$tags',$i);"
           
           # Employers section
           tempName=$(shuf -n 1 NAMES.txt | tr -d '\r'\') # Rolls a random name, truncates escape characters
           tempCompany=$(shuf -n 1 ACRONYMS.txt | tr -d '\r'\')" "$(shuf -n 1 PLACES.txt | tr -d '\r') # Rolls a random acronym + location
           tempEmail=$tempName"@"$(echo $tempCompany | tr -d ' ')".com" #Employer email
           
+          # Adds employer entries to employers table
           psql -U postgres -d arbonsi_db -c "INSERT INTO employers(employer_name,about_employer,company,employer_email,created_jobs) VALUES ('$tempName', 'Greatest employer on Earth.', '$tempCompany', '$tempEmail', '{$i}');"
+          
+          # Adds login info to login_imfo table
+          psql -U postgres -d arbonsi_db -c "INSERT INTO login_info(username, password, usertype, userid) VALUES ('$tempEmail', '123', '2', '$i')"
      done
      
-	 #First user insert to avoid breaking the website
+     #First user insert to avoid breaking the website
      psql -U postgres -d arbonsi_db -c "INSERT INTO students(student_name, school, student_email, about_me) VALUES ('Dennis', 'CU Boulder', 'test_email@gmail.com', 'Memes interlinked.')";
-     
      
      ## DEBUG ONLY
      # Prints out the inserted queries
      # psql -U postgres -d arbonsi_db -c "SELECT * FROM employers;"
-     # psql -U postgres -d arbonsi_db -c "SELECT id FROM job;"
+     # psql -U postgres -d arbonsi_db -c "SELECT id, tags FROM job;"
      # psql -U postgres -d arbonsi_db -c "SELECT * FROM jobListing_preview;"
      
 else
--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const bodyParser = require('body-parser'); // Add the body-parser tool has been 
 app.use(bodyParser.json());              // Add support for JSON encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // Add support for URL encoded bodies
 
+/* Tracks user types. 0 -- logged out, 1 -- student, 2 -- employer */
+var state = 0;
+var userId = 0;
 
 //Create Database Connection
 const pgp = require('pg-promise')();
@@ -16,28 +19,20 @@ const pgp = require('pg-promise')();
 /* 
 	NOTES
 		- Change the password in dbConfig to your current postgres password (it's under Lab_Website 3). The password has to be DISPOSABLE, don't commit personal passwords
-
-		- At the moment our server can't handle authentication, so student 1 and employer 1 are defaults for respective profiles
 */
 
 const dbConfig = {
 	host: 'localhost',
-	port: 53945,
+	port: 5432,
 	database: 'arbonsi_db',
 	user: 'postgres',
-	password: '123456'
+	password: 'J74?vW'
 };
 
 let db = pgp(dbConfig);
 
 //Serverside scripts
 //const scripts_node = require('./resources/js/scripts_node.js');
-
-/* Declaring renderers
-Query passes are to be restored later */
-app.get('/signin', function (req, res) {
-	res.render('pages/signin');
-});
 
 /* Main page request */
 app.get('/main', function (req, res) {
@@ -46,84 +41,55 @@ app.get('/main', function (req, res) {
 
 	db.any(query)
 		.then(function (rows) {
-			// console.log(rows)
-			console.log("main")
 			res.render('pages/main', {
 				mode: 0,
 				jobs: rows,
+				state: state
 			})
 		})
 		.catch(function (err) {
 			// Displays error message in case an error
 			res.render('pages/main', {
 				mode: 0,
-				jobs: ''
+				jobs: '',
+				state: state
 			})
 		})
 });
 
-/* sign in page */
-app.post('/login',function(req,res){
-	var username = req.body.username;
-  var password = req.body.password;
-	//console.log(username)
-	//console.log(password)
-	query = "select usertype,userid from login_info where username='" + username +"' and password = '" +password +"' ;"
-	db.any(query)
-		.then(function(rows) {
-			if(rows.length == 1){
-				//if info inerted was match with the database record
-				var usertype = rows[0].usertype;
-				var userid = rows[0].userid;
-				//usertype = 1 represent as student, then user_id is student_table's key [student_id]
-				//usertype = 2 represent as employer, then user_id is employer's key [employer]
-				console.log(rows[0])
-				if(usertype == '1' ){
-					query_student = "SELECT * FROM students WHERE student_id="+userid+";"
-					query_jobs = "SELECT * FROM job INNER JOIN students ON job.id=ANY(students.accepted_jobs) WHERE students.student_id="+userid+";"
+/* Sign in get */
+app.get('/signin', function (req, res) {
+	res.render('pages/signin', {
+		err: false,
+		state: state
+	});
+});
 
-					db.task('get-everything', task => {
-						return task.batch([
-							task.any(query_student),
-							task.any(query_jobs),
-						]);
-					})
-						.then(data => {
-							//console.log(data[1][0])
-							res.render('pages/student_profile', {
-								data_student: data[0][0],
-								data_jobs: data[1]
-							})
-						})
-				}else{
-					query_employer = "SELECT * FROM employers WHERE employer_id="+userid+";"
-					
-					db.task('get-everything', task => {
-						return task.batch([
-							task.any(query_employer),							
-						]);
-					})
-						.then(data => {
-							res.render('pages/employer_profile', {
-								query_employer: data[0][0],
-							})
-						})
-				}
-				/*res.render('pages/main',{
-					mode: 0,
-					jobs: ''
-					})*/
-			}else {
+/* Sign in post to verify login info */
+app.post('/signin', function (req, res) {
+	var username = req.body.username;
+	var password = req.body.password;
+
+	query = "select usertype,userid from login_info where username='" + username + "' and password = '" + password + "' ;"
+
+	db.any(query)
+		.then(function (rows) {
+			//Checks if there was a match in the database
+			if (rows.length == 1) {
+				state = rows[0].usertype;
+				userId = rows[0].userid;
+				res.redirect('/profile');
+
+			} else {
 				//if the info don't match, then send massage to user 
-				res.render('pages/signin',{
-					//�����Լ�д
+				res.render('pages/signin', {
+					err: true,
+					state: state
 				})
-				
 			}
 		})
-		.catch(function(err){
+		.catch(function (err) {
 			console.log(err)
-			//TODO
 		})
 });
 
@@ -135,49 +101,68 @@ app.get('/main/view', function (req, res) {
 
 	db.any(query)
 		.then(function (rows) {
-			// console.log(rows)
-			console.log("main/view")
 			res.render('pages/main', {
 				mode: 1,
 				my_title: "Main Page",
 				focusedJob: rows,
+				state: state
 			})
 		})
 		.catch(function (err) {
-			// Displays error message in case an error
 			res.render('pages/main', {
 				mode: 1,
 				title: 'Main Page',
-				focusedJob: ''
+				focusedJob: '',
+				state: state
 			})
 		})
 });
+
+/* Redirects to one of the profile types from here */
+app.get('/profile', function (req, res) {
+
+	if (state != 0) {
+		if (state == 1)
+			res.redirect('/student_profile');
+		else
+			res.redirect('/employer_profile');
+	}
+	else
+		res.redirect('/');
+})
 
 /* Student profile page */
 app.get('/student_profile', function (req, res) {
-	query_student = "SELECT * FROM students WHERE id=1;"
-	query_jobs = "SELECT * FROM job INNER JOIN students ON job.id=ANY(students.accepted_jobs) WHERE students.id=1;"
+	query_student = "SELECT * FROM students WHERE id='" + userId + "';";
+	query_jobs = "SELECT * FROM job INNER JOIN students ON job.id=ANY(students.accepted_jobs) WHERE students.id='" + userId + "';";
 
-	db.task('get-everything', task => {
-		return task.batch([
-			task.any(query_student),
-			task.any(query_jobs),
-		]);
-	})
-		.then(data => {
-			//console.log(data[1][0])
-			res.render('pages/student_profile', {
-				data_student: data[0][0],
-				data_jobs: data[1]
-			})
+	if (state == 1) {
+		db.task('get-everything', task => {
+			return task.batch([
+				task.any(query_student),
+				task.any(query_jobs),
+			]);
 		})
+			.then(data => {
+				res.render('pages/student_profile', {
+					data_student: data[0][0],
+					data_jobs: data[1],
+					state: state
+				})
+			})
+	}
+	else
+		res.redirect('/');
+
 });
 
 /* Student profile update */
-app.post('/student_profile/update', function(req, res){
-	query = "UPDATE students SET student_name='" + req.body.form_name + "', school='" + req.body.form_school + "', student_email='" + req.body.form_email + "', about_me='" + req.body.form_bio + "' WHERE id=1;"
+app.post('/student_profile/update', function (req, res) {
+	query0 = "UPDATE students SET student_name='" + req.body.form_name + "', school='" + req.body.form_school + "', student_email='" + req.body.form_email + "', about_me='" + req.body.form_bio + "' WHERE id='" + userId + "';";
+	query1 = "UPDATE login_info SET username='" + req.body.form_email + "' WHERE usertype='1' AND userid='" + userId + "';";
 
-	db.any(query);
+	db.any(query0);
+	db.any(query1);
 
 	res.redirect('/student_profile');
 });
@@ -186,38 +171,107 @@ app.post('/student_profile/update', function(req, res){
 Currently defaults to student 1 as the acceptor */
 app.post('/main/accept', function (req, res) {
 	//Updating array of accepted jobs for the student
-	query = "UPDATE students SET accepted_jobs = array_append(accepted_jobs,'" + req.body.job_id + "') WHERE id='1'";
+	query = "UPDATE students SET accepted_jobs = array_append(accepted_jobs,'" + req.body.job_id + "') WHERE id='" + userId + "'";
 	db.any(query)
 	res.redirect('/main')
 });
 
 app.get('/', function (req, res) {
-	res.render('pages/start_page')
+	res.render('pages/start_page', {
+		state: state
+	})
 });
 
 app.get('/signup_employer', function (req, res) {
-	res.render('pages/signup_employer');
+	res.render('pages/signup_employer', {
+		state: state
+	});
 });
 
 app.route('/signup_student')
 	.get((req, res) => {
-		res.render('pages/signup_student');
+		res.render('pages/signup_student', {
+			state: state
+		});
 	})
 	.post((req, res) => {
-		query = "INSERT INTO students(student_name, school, student_email) VALUES('" + req.body.full_name + "','" + req.body.school + "','" + req.body.email + "') ON CONFLICT DO NOTHING;";
-		db.any(query)
-		res.redirect('/main');
+		//Inserts student profile info
+		query0 = "INSERT INTO students(student_name, school, student_email) VALUES('" + req.body.full_name + "','" + req.body.school + "','" + req.body.email + "') ON CONFLICT DO NOTHING;";
+
+		//Inserts login info
+		query1 = "INSERT INTO login_info(username, password, usertype, userid) SELECT '" + req.body.email + "','" + req.body.password + "','1', id FROM students ORDER BY id DESC LIMIT 1;";
+
+		//Retrieves the new user id within the db
+		query2 = "SELECT id FROM students ORDER BY id DESC LIMIT 1;"
+
+		db.task('get-everything', task => {
+			return task.batch([
+				task.any(query0),
+				task.any(query1),
+				task.any(query2),
+			]);
+		})
+			//Technically useless, but I want to make sure I'm not redirecting in vain
+			.then(data => {
+				state = 1;
+				userId = data[2][0].id;
+				res.redirect('/profile');
+			})
+
+			.catch(function (err) {
+				res.redirect('/signup_student');
+			})
 	});
 
-
-app.post('/signup_student', function (req, res) {
-	res.render('pages/signup_student');
+/* Employer profile */
+app.get('/employer_profile', function (req, res) {
+	res.render('pages/employer_profile', {
+		state: state
+	})
 });
 
+/* Test request */
+app.post('/main/search', function (req, res) {
 
+	query = "SELECT * FROM job WHERE TRUE";
 
-app.get('/employer_profile', function (req, res) {
-	res.render('pages/employer_profile')
+	//Checking individual filter/search conditions
+	if (req.body.form_tag_0 == 'true')
+		query = query + " AND 'C#'=ANY(tags)";
+
+	if (req.body.form_tag_1 == 'true')
+		query = query + " AND 'JavaScript'=ANY(tags)";
+
+	if (req.body.form_tag_2 == 'true')
+		query = query + " AND 'LaTeX'=ANY(tags)";
+
+	if (parseInt(req.body.form_diff) > 0) //Pass if not 0
+		query = query + " AND '" + req.body.form_diff + "'<= difficulty";
+	if (req.body.form_job_name != '')
+		query = query + " AND title ~* '" + req.body.form_job_name + "'";
+	query = query + ";"
+
+	db.any(query)
+		.then(function (rows) {
+			res.render('pages/main', {
+				mode: 0,
+				jobs: rows,
+				state: state
+			})
+		})
+		.catch(function (err) {
+			// Displays error message in case an error
+			res.render('pages/main', {
+				mode: 0,
+				jobs: '',
+				state: state
+			})
+		})
+});
+
+app.get('/signout', function (req, res) {
+	state = 0;
+	res.redirect('/');
 });
 
 app.listen(3000);
